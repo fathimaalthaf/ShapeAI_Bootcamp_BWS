c1754505b07861e185cfedf8c3ca0ef589771b74
fathimaalthaf <80218153+fathimaalthaf@users.noreply.github.com>
2021-06-27 22:51:05 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  return (
+      <div class = "note">
+        <h1>Bootcamp highlights</h1>
+        <p> The bootcamp was well organised .The topic taught was precise and clear .An overall view of web  development using javascript and react.js got provided.</p>
+      </div>
+  );
+
+}
+
+export default Info;
